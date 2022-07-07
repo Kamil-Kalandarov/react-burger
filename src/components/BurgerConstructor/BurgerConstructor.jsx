a63978f54366531898aeb20dcd868daaffa7f1b1
Kamil Kalandarov <Unknownman131313@yandex.ru>
2022-07-07 23:34:22 +0300

    sort in procces

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -8,25 +8,88 @@ import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import Modal from '../Modal/Modal';
 import OrderDetails from '../OrderDetails/OrderDetails';
 import Preloader from '../Preloader/Preloader';
-import { postOrderNumber } from '../../services/actions/orderDetails';
 import { store } from '../../services/store';
 import { addIngredient } from '../../services/actions/burgerConstructor';
-import { deleteIngredient } from '../../services/actions/burgerConstructor';
+import { deleteIngredient, changeFillingPosition } from '../../services/actions/burgerConstructor';
 import { postOrder } from '../../services/actions/orderDetails';
-import { useDrop } from 'react-dnd';
+import { useDrop, useDrag } from 'react-dnd';
+import { useRef } from 'react';
 import EmptyConstructorElement from './EmptyConstructorElement/EmptyConstructorElement';
 
 
 /* Конструктор бургера */
-const BurgerConstructor = () => {
+const BurgerConstructor = ({ index }) => {
+  const dispatch = useDispatch();
+  
   const [isOrderDetailsOpened, setOrderDetailsOpened] = useState(false);
   const isLoading = useSelector(store => store.orderDetails.orederRequest); 
 
   const bun = useSelector(store => store.constructorIngredients.bun);
   const fillings = useSelector(store => store.constructorIngredients.fillings);
   const allConstructorIngredients = useSelector(store => store.constructorIngredients)
- 
-  const dispatch = useDispatch();
+
+  const id = fillings.forEach((filling) => filling.id)
+
+  const ref = useRef(null)
+
+  const [{ handlerId }, drop] = useDrop({
+    accept: 'newIndex',
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      }
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset()
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      // Time to actually perform the action
+      dispatch(changeFillingPosition(dragIndex, hoverIndex))
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    },
+  })
+
+  const [{ isDragging }, drag] = useDrag({
+    type: 'newIndex',
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
 
   const handleDrop = (ingredient) => {
     dispatch(addIngredient(ingredient))
@@ -37,7 +100,11 @@ const BurgerConstructor = () => {
   }
 
   const handleOrder =(orderedIngredients) => {
-    dispatch(postOrder(orderedIngredients))
+    dispatch(postOrder([
+      orderedIngredients.bun._id,
+      ...orderedIngredients.fillings.map((filling) => filling._id),
+      orderedIngredients.bun._id,
+    ]))
     setOrderDetailsOpened(true)
   }
 
@@ -47,7 +114,7 @@ const BurgerConstructor = () => {
 
   const [{ isHover }, dropTarget] = useDrop({
     accept: 'ingredient',
-    drop({ingredient}, monitor) {
+    drop({ ingredient }, monitor) {
       handleDrop(ingredient)
     },
     collect: (monitor) => ({
@@ -59,9 +126,10 @@ const BurgerConstructor = () => {
   
   return (
     <>
-      <section className={`${styles.burgerConstructor} pl-4`} style={{borderColor}} ref={dropTarget}>
+      <section className={`${styles.burgerConstructor} pl-4`} style={{ borderColor }} ref={dropTarget}>
         { bun ? (
-          <article key={bun.id} className={`${styles.burgerConstructor__cardBunElement} ml-8 mr-2 mt-25`}>
+          <article key={bun.id} className={`${styles.burgerConstructor__cardBunElement} ml-8 mr-2 mt-25`} 
+            style={{ opacity }} ref={drag} data-handler-id={ handlerId } index={index}>
             <ConstructorElement
               type="top"
               isLocked={true}
--- src/services/actions/burgerConstructor.jsx
+++ src/services/actions/burgerConstructor.jsx
@@ -2,6 +2,7 @@ import { nanoid } from "nanoid";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const CHANGE_FILLING_POSITION = 'CHANGE_FILLING_POSITION';
 export const RESET = 'RESET'
 
 export const addIngredient = (ingredient) => {
@@ -15,6 +16,16 @@ export const deleteIngredient = (id) => {
   return {
     type: DELETE_INGREDIENT,
     id
+  }
+}
+
+export const changeFillingPosition = (dragIndex, hoverIndex) => {
+  return {
+    type: CHANGE_FILLING_POSITION,
+    payload: {
+      from: dragIndex,
+      to: hoverIndex
+    }
   }
 }
 
