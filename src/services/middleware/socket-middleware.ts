bf3bb6a90cdc578aba4e043c4173727ccfc09db9
Kamil Kalandarov <Unknownman131313@yandex.ru>
2022-09-24 00:08:03 +0300

    second review

--- src/services/middleware/socket-middleware.ts
+++ src/services/middleware/socket-middleware.ts
@@ -1,22 +1,16 @@
 import { MiddlewareAPI, Middleware } from "redux";
 import { TWsActionsTypes } from "../store";
-import { 
-  WS_CONNECTION_START, 
-  WS_CONNECTION_SUCCESS,
-  WS_CONNECTION_FAILED,
-  WS_CONNECTION_CLOSED,
-  WS_GET_DATA 
-} from "../actions/ws";
     
 
 export const socketMiddleware = (wsActions: TWsActionsTypes): Middleware => {
+  const { wsStart, wsSuccess, wsFailed, wsClosed, wsGetData } = wsActions;
   return (store: MiddlewareAPI) => {
     let socket: WebSocket | null  = null;
     return (next) => (action) => {
       const { dispatch } = store;
       const { type, payload } = action;
 
-      if (type === WS_CONNECTION_START) {
+      if (type === wsStart) {
         console.log('wsStart');
         socket = new WebSocket(payload);
       }
@@ -24,24 +18,24 @@ export const socketMiddleware = (wsActions: TWsActionsTypes): Middleware => {
       if (socket) {
         socket.onopen = () => {
           console.log('wsOpened');
-          dispatch({ type: WS_CONNECTION_SUCCESS});
+          dispatch({ type: wsSuccess});
         };
 
         socket.onerror = (event) => {
           console.log('wsError');
-          dispatch({type: WS_CONNECTION_FAILED});
+          dispatch({type: wsFailed});
         };
 
         socket.onclose = () => {
           console.log('wsClosed');
-          dispatch({type: WS_CONNECTION_CLOSED});
+          dispatch({type: wsClosed});
         };
 
         socket.onmessage = event => {
           const { data } = event;
           const parsedData = JSON.parse(data);
           const { success, ...restParsedData } = parsedData;
-          dispatch({type: WS_GET_DATA, payload: restParsedData});
+          dispatch({type: wsGetData, payload: restParsedData});
         };
       }
       next(action);
