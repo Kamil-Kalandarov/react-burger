c63596bcaace2c933bef8e9241c3567104531bb1
Kamil Kalandarov <Unknownman131313@yandex.ru>
2022-10-20 00:34:20 +0500

    hasRouter added

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './services/store';
 
 
 createRoot(document.getElementById('root') as HTMLDivElement).render (
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
 
